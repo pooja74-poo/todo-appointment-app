ab574528afea0a173c63df426916a59872b9d49e
Pooja <godavarthipooja1999@gmail.com>
2025-05-01 23:20:56 +0530

    Updated code

--- src/todo-login.jsx
+++ src/todo-login.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { FaUser, FaLock } from "react-icons/fa";
 
 export function ToDoLogin() {
-  const [cookies, setCookie] = useCookies(["userid"]);
+  const [, setCookie] = useCookies(["userid"]);
   let navigate = useNavigate();
 
   const formik = useFormik({
